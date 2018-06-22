b1eb92793385d47ec0e7b97706f157bf2b382ba2
kritpunpreuk <33280952+kritpunpreuk@users.noreply.github.com>
2018-06-22 09:49:08 +0700

    tanrabad casecount reader

---
+++ R/hello.R
@@ -0,0 +1,37 @@
+#' Tanrabad casecount reader
+#'
+#' @param txt casecount json text
+#' @return data frame table
+#' @examples
+#' library(Rbigstream)
+#' host <- "http://sample.bigstream.io/"
+#' storage_name <- "sample.trb"
+#' token <- "token"
+#' bs.connect(host, storage_name, token)
+#' json <- storage.get(storage_name,flatten=FALSE)
+#' df <- trb.readcasecount(json)
+#' @export
+
+trb.readcasecount <- function(txt) {
+  json_data %>%
+    as.tbl_json %>%
+    enter_object("data") %>%
+    spread_values(
+      disease_code = jstring("disease_code"),
+      disease_name = jstring("disease_name")
+    ) %>%
+    enter_object("data") %>%
+    gather_array %>%
+    spread_values(
+      province.code = jstring("province_code"),
+      province.name = jstring("province_name"),
+      province.namt = jstring("province_namt")
+    ) %>%
+    enter_object("data")%>%
+    gather_array %>%
+    spread_values(
+      week_of_year = jnumber("week_of_year"),
+      year = jnumber("year"),
+      case_count = jnumber("case_count")
+    )
+}
